1bcc13a891749bb84e14f665529d383b0846c10d
saqibk999 <saqib.mohd1@byjus.com>
2023-02-06 21:46:02 +0530

    bugs fixed

--- DomAssignmentQ2/script.js
+++ DomAssignmentQ2/script.js
@@ -1,16 +1,20 @@
 const btn = document.getElementById('btn');
 const api_url = "https://jsonplaceholder.typicode.com/users/1";
 let list = document.getElementById("list");
+const card=document.getElementById('card');
 
 btn.addEventListener('click', function handleClick() {
   btn.textContent = 'Button is Clicked';
   btn.style.backgroundColor= 'magenta';
   btn.style.borderColor='purple';
-  getapi(api_url);
+  event.stopPropagation();
 });
 
+card.addEventListener('click', function handleClick(){
+  console.log("card clicked");
+  getapi(api_url);
+});
   
-// Defining async function
 async function getapi(url) {
     
     fetch(url)
